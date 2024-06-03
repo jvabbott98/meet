7f5f6b5ac8433a8df82e594eba8d810febbdb161
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-06-03 11:52:06 -0400

    added offline capability

--- src/api.js
+++ src/api.js
@@ -1,67 +1,74 @@
 import mockData from './mock-data';
 
 export const extractLocations = (events) => {
-    const extractedLocations = events.map((event) => event.location);
-    const locations = [...new Set(extractedLocations)];
-    return locations;
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
 };
 
 const getToken = async (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-        "https://mo7x7mboq4.execute-api.us-east-1.amazonaws.com/dev/api/token" + '/' + encodeCode
-    );
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-  
-    return access_token;
-  };
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    "https://mo7x7mboq4.execute-api.us-east-1.amazonaws.com/dev/api/token" + '/' + encodeCode
+  );
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
 
 const removeQuery = () => {
-    let newurl;
-    if (window.history.pushState && window.location.pathname) {
-      newurl =
-        window.location.protocol +
-        "//" +
-        window.location.host +
-        window.location.pathname;
-      window.history.pushState("", "", newurl);
-    } else {
-      newurl = window.location.protocol + "//" + window.location.host;
-      window.history.pushState("", "", newurl);
-    }
-  };
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
 
 const checkToken = async (accessToken) => {
-    const response = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    );
-    const result = await response.json();
-    return result;
-  };
-  
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
 
-  export const getEvents = async () => {
-    if (window.location.href.startsWith("http://localhost")) {
-      return mockData;
-    }
+
+export const getEvents = async () => {
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
   
-    const token = await getAccessToken();
-  
-    if (token) {
-      removeQuery();
-      const url =  "https://mo7x7mboq4.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
-      const response = await fetch(url);
-      const result = await response.json();
-      if (result) {
-        return result.events;
-      } else return null; 
-    }
-  };
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = "https://mo7x7mboq4.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
+      return result.events;
+    } else return null;
+  }
+};
 
 export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-    const tokenCheck = accessToken && (await checkToken(accessToken));
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
   if (!accessToken || tokenCheck.error) {
     await localStorage.removeItem("access_token");
